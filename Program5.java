f21fc51ec4d16cb7145bfac28b2b726edce3a01b
gamer6700k <62044986+gamer6700k@users.noreply.github.com>
2024-08-20 22:20:18 +0000

    some progress in class

--- Program5.java
+++ Program5.java
@@ -1,7 +1,27 @@
 //import stuff here?
 
 //Your code here
+public class Program5 {
+    public static void main(String[] args) {
+        
+        int royaleMiles = 
+        int royaleGallons =
+        int royaleMPG = 0; 
+        int koopakingMiles = 
+        int koopakingGallons =
+        int koopakingMPG = 0; 
+        int pipeframeMiles = 
+        int pipeframeGallons =
+        int pipeframeMPG = 0; 
+        int badwagonMiles = 
+        int badwagonGallons =
+        int badwagonMPG = 0; 
+    }
 
+
+
+
+}
 //Paste console output below:
 /*
 
