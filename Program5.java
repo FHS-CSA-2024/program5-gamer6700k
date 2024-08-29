6c14cbe8119ce64910af1977c71a2064b473a09b
gamer6700k <62044986+gamer6700k@users.noreply.github.com>
2024-08-29 23:57:24 +0000

    more work

--- Program5.java
+++ Program5.java
@@ -3,20 +3,32 @@
 //Your code here
 public class Program5 {
     public static void main(String[] args) {
-        
-        int royaleMiles = 
-        int royaleGallons =
+        int mpg = 0;
+        int royaleMiles = 286;
+        int royaleGallons = 9;
         int royaleMPG = 0; 
-        int koopakingMiles = 
-        int koopakingGallons =
+        int koopakingMiles = 412;
+        int koopakingGallons = 40;
         int koopakingMPG = 0; 
-        int pipeframeMiles = 
-        int pipeframeGallons =
+        int pipeframeMiles = 361;
+        int pipeframeGallons = 18
         int pipeframeMPG = 0; 
-        int badwagonMiles = 
-        int badwagonGallons =
-        int badwagonMPG = 0; 
+        int badwagonMiles = 161;
+        int badwagonGallons = 11;
+        int badwagonMPG = 0;
+        mpg = (double) royaleMiles/royaleGallons 
+        mpg = mpg * 10 
+        mpg = Math.round(average);
+        mpg = mpg/10
+        System.out.println("Royale averaged " + mpg + "m/g/n");
+
+
+
+
+
+
     }
+
 
 
 
